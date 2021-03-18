72bdbfe38c783bb098a5241b7409fd15e2a89388
Vladimir <woaouh@outlook.com>
2021-03-18 12:03:24 +0200

    Implemented CheckoutContainer with react router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar';
 import CheckoutContainer from '../CheckoutContainer';
-import BreadCrumbs from '../BreadCrumbs';
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <CheckoutContainer>
-        <BreadCrumbs />
-      </CheckoutContainer>
+      <CheckoutContainer />
     </>
   );
 }
--- src/components/CheckoutContainer/index.jsx
+++ src/components/CheckoutContainer/index.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './index.module.sass';
+import { Route, Switch } from 'react-router';
 import Container from '../Container';
+import BreadCrumbs from '../BreadCrumbs';
+import ShippingForm from '../ShippingForm';
+import BillingForm from '../BillingForm';
+import PaymentForm from '../PaymentForm';
+import styles from './index.module.sass';
+import CompletedOrder from '../CompletedOrder';
 
-export default function CheckoutContainer({ children }) {
+export default function CheckoutContainer() {
   return (
     <Container>
-      <div className={styles.checkoutContainer}>{children}</div>
+      <div className={styles.checkoutContainer}>
+        <div className={styles.right}>
+          <Switch>
+            <Route exact path="/">
+              <BreadCrumbs />
+              <ShippingForm />
+            </Route>
+            <Route path="/billing">
+              <BreadCrumbs />
+              <BillingForm />
+            </Route>
+            <Route path="/payment">
+              <BreadCrumbs />
+              <PaymentForm />
+            </Route>
+            <Route path="/completed-order">
+              <BreadCrumbs />
+              <CompletedOrder />
+            </Route>
+          </Switch>
+        </div>
+        <div className={styles.left}>
+          Products
+        </div>
+      </div>
     </Container>
   );
 }
-
-CheckoutContainer.propTypes = {
-  children: PropTypes.element.isRequired,
-};
