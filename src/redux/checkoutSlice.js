82c4b8356c492953ac625687950323ece2e5c7ff
Vladimir <woaouh@outlook.com>
2021-03-18 12:04:26 +0200

    Implemented checkoutSlice actions for collecting form data from checkout process

--- src/redux/checkoutSlice.js
+++ src/redux/checkoutSlice.js
@@ -4,10 +4,46 @@ import { createSlice } from '@reduxjs/toolkit';
 export const checkoutSlice = createSlice({
   name: 'checkout',
   initialState: {
-    activeStage: 'Shipping',
+    userInfo: {
+      name: '',
+      phone: '',
+      address: '',
+      apartment: '',
+      city: '',
+      country: '',
+      zip: '',
+    },
+    billingInfo: {
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      apartment: '',
+      city: '',
+      country: '',
+      zip: '',
+    },
+    paymentInfo: {
+      cardholderName: '',
+      cardNumber: '',
+      expireDate: '',
+      securityCode: '',
+    },
   },
-  reducers: {},
+  reducers: {
+    setShippingInfo(state, action) {
+      state.userInfo = action.payload;
+    },
+    setBillingInfo(state, action) {
+      state.billingInfo = action.payload;
+    },
+    setPaymentInfo(state, action) {
+      state.paymentInfo = action.payload;
+    },
+  },
   extraReducers: {},
 });
+
+export const { setShippingInfo, setBillingInfo, setPaymentInfo } = checkoutSlice.actions;
 
 export default checkoutSlice.reducer;
