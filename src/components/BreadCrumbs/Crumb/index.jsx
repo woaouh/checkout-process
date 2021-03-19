2a2cdd02ad5e98089915682665913cd703343976
Vladimir <woaouh@outlook.com>
2021-03-19 14:36:58 +0200

    Removed react-router from BreadCrumbs

--- src/components/BreadCrumbs/Crumb/index.jsx
+++ src/components/BreadCrumbs/Crumb/index.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
 import styles from './index.module.sass';
 
-export default function Crumb({ stage, route }) {
-  return (
-    <li key={stage} className={styles.crumb}>
-      <NavLink to={route} activeClassName={styles.active} exact>
-        {stage}
-      </NavLink>
-    </li>
-  );
+export default function Crumb({ stage }) {
+  return <li key={stage} className={styles.crumb}>{stage}</li>;
 }
 
 Crumb.propTypes = {
   stage: PropTypes.string.isRequired,
-  route: PropTypes.string.isRequired,
 };
--- src/components/BreadCrumbs/index.jsx
+++ src/components/BreadCrumbs/index.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import Crumb from './Crumb';
 import styles from './index.module.sass';
 
-const stages = [
-  { name: 'Shipping', route: '/' },
-  { name: 'Billing', route: '/billing' },
-  { name: 'Payment', route: '/payment' },
-];
+const stages = ['Shipping', 'Billing', 'Payment'];
 
 export default function BreadCrumbs() {
   return (
     <ul className={styles.breadCrumbs}>
       {stages.map((stage) => (
-        <Crumb key={stage.name} stage={stage.name} route={stage.route} />
+        <Crumb key={stage} stage={stage} />
       ))}
     </ul>
   );
