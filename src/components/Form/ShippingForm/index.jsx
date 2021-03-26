b51ebacc94780930d07fefe5cd6d4829a23bb1a5
Vladimir <woaouh@outlook.com>
2021-03-26 18:19:13 +0200

    Changed Sass file to SCSS in ShippingForm component

--- src/components/Form/ShippingForm/index.jsx
+++ src/components/Form/ShippingForm/index.jsx
@@ -5,7 +5,7 @@ import { useHistory } from 'react-router';
 import Button from '../../UI/Button';
 import Input from '../../UI/Input';
 import Select from '../../UI/Select';
-import styles from './index.module.sass';
+import classes from './index.module.scss';
 
 import COUNTRIES from '../../../assets/countries';
 import { setShippingInfo } from '../../../redux/checkoutSlice';
@@ -22,11 +22,11 @@ export default function ShippingForm() {
   }
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit(onSubmitHandler)}>
+    <form className={classes.form} onSubmit={handleSubmit(onSubmitHandler)}>
       <h2>Shipping Info</h2>
-      <p className={styles.label}>Recipient</p>
+      <p className={classes.label}>Recipient</p>
       <Input type="text" name="name" placeholder="Full Name" register={register} required />
-      <div className={styles.phoneContainer}>
+      <div className={classes.phone_container}>
         <Input type="tel" name="phone" placeholder="Daytime Phone" register={register} required />
         <p>
           For delivery
@@ -34,11 +34,11 @@ export default function ShippingForm() {
           questions only
         </p>
       </div>
-      <p className={styles.label}>Address</p>
+      <p className={classes.label}>Address</p>
       <Input type="text" name="address" placeholder="Street Address" register={register} required />
       <Input type="text" name="apartment" placeholder="Apt, Suite, Bldg, Gate Code. (optional)" register={register} required />
       <Input type="text" name="city" placeholder="City" register={register} required />
-      <div className={styles.inputContainer}>
+      <div className={classes.input_container}>
         <Select name="country" options={COUNTRIES} register={register} required />
         <Input type="number" name="zip" placeholder="ZIP" register={register} required />
       </div>
