2268605e94817ba1cc885b1e8aea23db8dced0d6
Vladimir <woaouh@outlook.com>
2021-03-26 18:20:45 +0200

    Changed Sass file to SCSS in CompleteOrder component

--- src/components/Form/CompletedOrder/index.jsx
+++ src/components/Form/CompletedOrder/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import styles from './index.module.sass';
+import classes from './index.module.scss';
 
 export default function CompletedOrder() {
   const userEmail = useSelector(({ checkout }) => checkout.billingInfo.email);
 
   return (
-    <div className={styles.completedOrder}>
-      <p className={styles.title}>Thank you for your order!</p>
-      <p className={styles.orderNumber}>Your order number is: 188787788</p>
-      <p className={styles.email}>
+    <div className={classes.completed_order}>
+      <p className={classes.title}>Thank you for your order!</p>
+      <p className={classes.orderNumber}>Your order number is: 188787788</p>
+      <p className={classes.email}>
         Your will recieve an email confirmation shortly to
         {' '}
         <span>{userEmail}</span>
@@ -17,9 +17,9 @@ export default function CompletedOrder() {
       <p>
         Estimated delivery Day is
         {' '}
-        <span className={styles.date}>Friday 1st April 2016</span>
+        <span className={classes.date}>Friday 1st April 2016</span>
       </p>
-      <button type="button" className={styles.btn}>Print Recipe</button>
+      <button type="button" className={classes.btn}>Print Recipe</button>
     </div>
   );
 }
