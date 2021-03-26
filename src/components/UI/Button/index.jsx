1779d10ba0ba088ea05c6c3131b281330eac4b19
Vladimir <woaouh@outlook.com>
2021-03-26 18:52:14 +0200

    Expanded button Component with one more vision

--- src/components/UI/Button/index.jsx
+++ src/components/UI/Button/index.jsx
@@ -1,19 +1,35 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
 import classes from './index.module.scss';
 
-export default function Button({ type, children, handler }) {
-  return <button type={type} onClick={handler} className={classes.button}>{children}</button>;
+export default function Button({
+  type, children, handler, simple,
+}) {
+  return (
+    <button
+      type={type}
+      onClick={handler}
+      className={classNames(classes.button, {
+        [classes.simple]: simple,
+      })}
+    >
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
   type: PropTypes.string,
   children: PropTypes.string.isRequired,
   handler: PropTypes.func,
+  simple: PropTypes.bool,
 };
 
 Button.defaultProps = {
   type: 'button',
-  handler: (() => null),
+  handler: () => null,
+  simple: false,
 };
