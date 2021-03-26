26f2a81a1d49d400404f756f41cf5d3c1b724684
Vladimir <woaouh@outlook.com>
2021-03-26 18:12:14 +0200

    Changed Sass files with global styles to SCSS

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
 import App from './components/App';
 
-import './index.module.sass';
+import './index.module.scss';
 
 ReactDOM.render(
   <React.StrictMode>
