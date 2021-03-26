840c547710748e7a2926e834be617504c634a86d
Vladimir <woaouh@outlook.com>
2021-03-26 18:13:57 +0200

    Changed Sass file to SCSS in CheckoutContainer component

--- src/components/CheckoutContainer/index.jsx
+++ src/components/CheckoutContainer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import Container from '../Container';
 import BreadCrumbs from '../BreadCrumbs';
-import styles from './index.module.sass';
+import classes from './index.module.scss';
 import ShippingForm from '../Form/ShippingForm';
 import BillingForm from '../Form/BillingForm';
 import PaymentForm from '../Form/PaymentForm';
@@ -12,15 +12,15 @@ import OrderSummary from '../OrderSummary';
 export default function CheckoutContainer() {
   return (
     <Container>
-      <section className={styles.checkoutContainer}>
-        <div className={styles.right}>
+      <section className={classes.checkout_container}>
+        <div className={classes.right}>
           <BreadCrumbs />
           <Route exact path="/" component={ShippingForm} />
           <Route path="/billing" component={BillingForm} />
           <Route path="/payment" component={PaymentForm} />
           <Route path="/completed-order" component={CompletedOrder} />
         </div>
-        <div className={styles.left}>
+        <div className={classes.left}>
           <OrderSummary />
         </div>
       </section>
