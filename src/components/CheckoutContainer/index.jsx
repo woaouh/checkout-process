49f2825ee0cb0aebc095cf3b1f9fa718cf2ecc17
Vladimir <woaouh@outlook.com>
2021-03-31 22:37:00 +0300

    Moved all the form steps components from CheckoutContainer to new Form container

--- src/components/CheckoutContainer/index.jsx
+++ src/components/CheckoutContainer/index.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, Route, useHistory } from 'react-router';
 
 import Container from '../Container';
 import BreadCrumbs from '../BreadCrumbs';
-import ShippingForm from '../Form/ShippingForm';
-import BillingForm from '../Form/BillingForm';
-import PaymentForm from '../Form/PaymentForm';
-import CompletedOrder from '../Form/CompletedOrder';
+import Form from '../Form';
 import OrderSummary from '../OrderSummary';
-
-import { setUserInfo } from '../../redux/checkoutSlice';
 
 import classes from './index.module.scss';
 
@@ -30,39 +23,13 @@ const steps = [
 ];
 
 export default function CheckoutContainer() {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const { activeStep } = useSelector(({ checkout }) => checkout);
-
-  function onFormSubmit(data) {
-    dispatch(setUserInfo(data));
-    history.push(steps[activeStep + 1].path);
-  }
-
-  function renderRoute(step, component) {
-    return (
-      <Route path={steps[step].path}>
-        {activeStep !== step ? <Redirect to="/" /> : component}
-      </Route>
-    );
-  }
-
-  const formSteps = [
-    <ShippingForm onSubmit={onFormSubmit} />,
-    <BillingForm onSubmit={onFormSubmit} />,
-    <PaymentForm onSubmit={onFormSubmit} />,
-    <CompletedOrder />,
-  ];
-
   return (
     <Container>
       <section className={classes.checkout_container}>
-
         <div className={classes.right}>
           <BreadCrumbs steps={steps.slice(0, -1)} />
-          {formSteps.map((component, i) => renderRoute(i, component))}
+          <Form steps={steps} />
         </div>
-
         <div className={classes.left}>
           <OrderSummary />
         </div>
