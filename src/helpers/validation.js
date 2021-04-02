d637b41da7fc079c3470a22f8821fc3443bb95f8
Vladimir <woaouh@outlook.com>
2021-04-02 13:47:17 +0300

    Added yup and @hook/resolvers packages to the project for handling form validation

---
+++ src/helpers/validation.js
@@ -0,0 +1,60 @@
+import * as yup from 'yup';
+
+const nameRegExp = /[a-zA-Z][a-zA-Z ]{1,}/;
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const cardRegExp = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
+const cardExpDateRegExp = /^(0[1-9]|1[0-2])\/\d{2}$/;
+
+const schema = {
+  name: yup.string()
+    .required('Name is required')
+    .matches(nameRegExp, 'Name must be more then 2 letters'),
+  phone: yup.string()
+    .required('Phone is required')
+    .min(5, 'Phone must be more then 5 numbers')
+    .matches(phoneRegExp, 'Phone must be a valid phone number'),
+  email: yup.string()
+    .required('Email is required')
+    .email('Email must be a valid email'),
+  address: yup.string()
+    .required('Address is required'),
+  city: yup.string()
+    .required('City is required'),
+  country: yup.string()
+    .required('Country is required'),
+  postcode: yup.string()
+    .required('ZIP code is required'),
+};
+
+export const shippingSchema = yup.object().shape({
+  name: schema.name,
+  phone: schema.phone,
+  address: schema.address,
+  city: schema.city,
+  country: schema.country,
+  postcode: schema.postcode,
+});
+
+export const billingSchema = yup.object().shape({
+  name: schema.name,
+  email: schema.email,
+  address: schema.address,
+  city: schema.city,
+  country: schema.country,
+  postcode: schema.postcode,
+});
+
+export const paymentSchema = yup.object().shape({
+  'cc-name': yup.string()
+    .required('Name is required')
+    .matches(nameRegExp, 'Name must be Name and Surname'),
+  'cc-number': yup.string()
+    .required('Card number is required')
+    .matches(cardRegExp, 'Card number must be valid'),
+  'cc-exp': yup.string()
+    .required('Expiration date is required')
+    .matches(cardExpDateRegExp, 'Expiration date must be MM/YY'),
+  'cc-csc': yup.string()
+    .required('CSC is required')
+    .length(3, 'CSC number must be valid'),
+});
